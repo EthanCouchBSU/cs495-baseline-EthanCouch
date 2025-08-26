5253a6ad26f57b38635c066205c44f895dd4c972
EthanCouchBSU <143107342+EthanCouchBSU@users.noreply.github.com>
2025-08-26 15:27:20 -0400

    Added 4 unit tests, check for empty input, no matches, one occruance, and multiple occurances

--- test-utils.py
+++ test-utils.py
@@ -1,7 +1,33 @@
+from unittest.mock import patch
 from utils import *
+import unittest
 
 
-test_List = ["apple","orange","pear"]
+class TestStringReplacementMethod(unittest.TestCase):
 
-def test_NoMatches():
-    assert replace_SubString_Randomly("[]", "Hello World", test_List) == "Hello World"
+    @patch('utils.random.choice')
+    def test_NoMatches(self,unrandom_Value):
+        unrandom_Value.return_value = "apple"
+        result = replace_SubString_Randomly("[]", "Hello World", ["apple","orange","pear"])
+        self.assertEqual(result, "Hello World")
+
+    def test_Empty_Input(self):
+        with self.assertRaises(ValueError):
+            replace_SubString_Randomly("[]", None, ["apple","orange","pear"])
+        
+
+
+    @patch('utils.random.choice')
+    def test_One_Replace(self,unrandom_Value):
+        unrandom_Value.return_value = "apple"
+        result = replace_SubString_Randomly("[]", "[] Hello World", ["apple","orange","pear"])
+        self.assertEqual(result, "apple Hello World")
+
+    @patch('utils.random.choice')
+    def test_Multiple_Replace(self,unrandom_Value):
+        unrandom_Value.return_value = "apple"
+        result = replace_SubString_Randomly("[]", "[] [] [] Hello World", ["apple","orange","pear"])
+        self.assertEqual(result, "apple apple apple Hello World")
+
+if __name__ == 'main':
+    unittest.main()
--- utils.py
+++ utils.py
@@ -5,5 +5,7 @@ import random
 # a function to replace each instance of a substring within an input string with a 
 # randomly picked string from a predetermined array of strings.
 def replace_SubString_Randomly(substring,input,replace_List):
+    if input is None:
+        raise ValueError("Input is null")
     return input.replace(substring, random.choice(replace_List))
         
